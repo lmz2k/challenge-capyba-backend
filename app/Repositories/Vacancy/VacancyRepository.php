89263ab7b0b0ab2c754dc17b44d20ffb3a18312a
lmz2k <gabriel1997lima@gmail.com>
2021-08-28 17:39:45 -0300

    creating private methods to filter list

--- app/Repositories/Vacancy/VacancyRepository.php
+++ app/Repositories/Vacancy/VacancyRepository.php
@@ -3,11 +3,86 @@
 
 namespace App\Repositories\Vacancy;
 
+use App\Models\Vacancy;
 
 class VacancyRepository implements VacancyRepositoryInterface
 {
-    public function show(): array
+    public function getVacanciesList(
+        $search,
+        $hiringMode,
+        $occupation,
+        $isHomeOffice,
+        $cityId,
+        $salary,
+        $createdAt,
+        $page,
+        $perPage
+    ) {
+        $query = Vacancy::leftJoin('users', 'users.id', '=', 'vacancies.announcement_by')
+            ->leftJoin('cities', 'cities.id', '=', 'vacancies.city_id')
+            ->leftJoin('states', 'states.id', '=', 'cities.state_id');
+
+        $this->filterBySearch($query, $search);
+        $this->filterByHiringMode($query, $hiringMode);
+        $this->filterByOccupation($query, $occupation);
+        $this->filterByLocal($query, $isHomeOffice, $cityId);
+        $this->orderByColumn($query, $salary, 'salary');
+        $this->orderByColumn($query, $createdAt, 'created_at');
+
+
+        return $query->paginate($perPage, ['*'], 'page', $page);
+    }
+
+    private function filterBySearch(&$query, $search)
     {
-        return ['hey' => 'tei'];
+        if (!isset($search)) {
+            return;
+        }
+
+        $query->where(
+            function ($subQuery) use ($search) {
+                $subQuery->where('vacancies.title', 'like', '%' . $search . '%')
+                    ->orWhere('vacancies.description', 'like', '%' . $search . '%')
+                    ->orWhere('cities.name', 'like', '%' . $search . '%');
+            }
+        );
+    }
+
+    private function filterByHiringMode(&$query, $hiringMode)
+    {
+        if (!isset($hiringMode)) {
+            return;
+        }
+
+        if ($hiringMode !== Vacancy::BOTH_HIRING) {
+            $query->where('vacancies.hiring_mode', '=', $hiringMode);
+        }
+    }
+
+    private function filterByOccupation(&$query, $occupation)
+    {
+        if (!isset($occupation)) {
+            return;
+        }
+
+        $query->where('vacancies.occupation', '=', $occupation);
+    }
+
+    private function filterByLocal(&$query, $isHomeOffice, $cityId)
+    {
+        if (isset($isHomeOffice)) {
+            $query->where('vacancies.is_home_office', '=', $isHomeOffice);
+        }
+
+        if (isset($cityId)) {
+            $query->where('vacancies.city_id', '=', $cityId);
+        }
+    }
+
+    private function orderByColumn(&$query, $order, $column)
+    {
+        $selectedOrder = $order ?? 'asc';
+
+        $query->orderBy('vacancies'.$column, $selectedOrder);
     }
 }
