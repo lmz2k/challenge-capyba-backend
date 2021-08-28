e5aae79483d071b9d2d6e4e592a502b69c4e74a0
lmz2k <gabriel1997lima@gmail.com>
2021-08-28 15:56:06 -0300

    binding service and repository

--- app/Http/Controllers/VacanciesController.php
+++ app/Http/Controllers/VacanciesController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Services\Vacancy\VacancyServiceInterface;
 
 class VacanciesController extends Controller
 {
+    private VacancyServiceInterface $vacancyService;
 
+    public function __construct( VacancyServiceInterface $vacancyService)
+    {
+        $this->vacancyService = $vacancyService;
+    }
 
     public function show(Request $request): JsonResponse
     {
-        return response()->json(['hey' => 'tei'], 200);
+        $result = $this->vacancyService->show();
+        return response()->json($result, 200);
     }
 }
