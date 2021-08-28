b24070930f6573c917a861ec635a26d10b29c3b2
lmz2k <gabriel1997lima@gmail.com>
2021-08-28 13:48:59 -0300

    Create city migration

---
+++ database/migrations/2021_08_28_140443_create_cities_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Cities', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('name')
+                ->nullable(false);
+
+            $table->integer('state_id')
+                ->unsigned();
+
+            $table->foreign('state_id')
+                ->references('id')
+                ->on('States');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Cities');
+    }
+}
