8100a4d5b06aed78e38ca1969294cbe08f385732
lmz2k <gabriel1997lima@gmail.com>
2021-08-29 12:50:09 -0300

    binding new services and repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\Auth\AuthRepository;
+use App\Repositories\Auth\AuthRepositoryInterface;
+use App\Services\Auth\AuthService;
+use App\Services\Auth\AuthServiceInterface;
+use App\Services\Ftp\FtpService;
+use App\Services\Ftp\FtpServiceInterface;
 use Illuminate\Support\ServiceProvider;
 use App\Services\Vacancy\VacancyService;
 use App\Services\Vacancy\VacancyServiceInterface;
@@ -26,6 +32,21 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             VacancyRepositoryInterface::class,
             VacancyRepository::class,
+        );
+
+        $this->app->bind(
+            AuthServiceInterface::class,
+            AuthService::class,
+        );
+
+        $this->app->bind(
+            AuthRepositoryInterface::class,
+            AuthRepository::class,
+        );
+
+        $this->app->bind(
+            FtpServiceInterface::class,
+            FtpService::class,
         );
     }
 }
