931de99a967bf9e9f825e47863891edc0f1be0d7
lmz2k <gabriel1997lima@gmail.com>
2021-08-29 11:29:08 -0300

    Creating repository to get, create and update vacancy

--- app/Repositories/Vacancy/VacancyRepository.php
+++ app/Repositories/Vacancy/VacancyRepository.php
@@ -54,6 +54,95 @@ class VacancyRepository implements VacancyRepositoryInterface
             $page);
     }
 
+    public function getVacancy($vacancyId)
+    {
+        return Vacancy::leftJoin('users', 'users.id', '=', 'vacancies.announcement_by')
+            ->leftJoin('cities', 'cities.id', '=', 'vacancies.city_id')
+            ->leftJoin('states', 'states.id', '=', 'cities.state_id')
+            ->where('vacancies.id', $vacancyId)
+            ->firstOrFail(
+                [
+                    'vacancies.id as id',
+                    'vacancies.title as title',
+                    'vacancies.description as description',
+                    'vacancies.is_home_office as home_office',
+                    'vacancies.occupation as occupation',
+                    'vacancies.salary as salary',
+                    'cities.name as city',
+                    'states.name as state',
+                    DB::raw(
+                        "CASE
+                        WHEN vacancies.hiring_mode = 'BOTH'
+                        THEN 'PJ E CLT'
+                        ELSE vacancies.hiring_mode
+                     END as hiring_mode"
+                    )
+                ]
+            );
+    }
+
+    public function createVacancy(
+        $userId,
+        $title,
+        $description,
+        $salary,
+        $isHomeOffice,
+        $occupation,
+        $cityId,
+        $hiringMode
+    ): Vacancy
+    {
+        $vacancy = new Vacancy();
+
+        $vacancy->title = $title;
+        $vacancy->description = $description;
+        $vacancy->salary = $salary;
+        $vacancy->is_home_office = $isHomeOffice;
+        $vacancy->occupation = $occupation;
+        $vacancy->city_id = $cityId;
+        $vacancy->hiring_mode = $hiringMode;
+        $vacancy->announcement_by = $userId;
+
+        $vacancy->save();
+
+        return $vacancy;
+    }
+
+    public function updateVacancy(
+        $id,
+        $title,
+        $description,
+        $salary,
+        $isHomeOffice,
+        $occupation,
+        $cityId,
+        $hiringMode
+    ): Vacancy
+    {
+        $vacancy = new Vacancy();
+        $vacancy->exists = true;
+        $vacancy->id = $id;
+
+        $this->updateVacancyColumn($vacancy, $title, 'title');
+        $this->updateVacancyColumn($vacancy, $description, 'description');
+        $this->updateVacancyColumn($vacancy, $salary, 'salary');
+        $this->updateVacancyColumn($vacancy, $isHomeOffice, 'is_home_office');
+        $this->updateVacancyColumn($vacancy, $occupation, 'occupation');
+        $this->updateVacancyColumn($vacancy, $cityId, 'city_id');
+        $this->updateVacancyColumn($vacancy, $hiringMode, 'hiring_mode');
+
+        $vacancy->save();
+
+        return $vacancy;
+    }
+
+    private function updateVacancyColumn(&$vacancy, $value, $key)
+    {
+        if ($value) {
+            $vacancy->{$key} = $value;
+        }
+    }
+
     private function filterListBySearch(&$query, $search)
     {
         if (!isset($search)) {
