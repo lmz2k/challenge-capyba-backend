d8dcc99a017b025e7dd57230f272fcc44f921dd6
lmz2k <gabriel1997lima@gmail.com>
2021-08-29 11:41:26 -0300

    Update vacancies response codes

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,23 @@
 
 
 $router->group(['prefix' => 'api'], function () use ($router) {
+        $router->get('/check', function () use ($router) {
+                return $router->app->version();
+            }
+        );
 
-    $router->get('/check', function () use ($router) {
-        return $router->app->version();
-    });
+        $router->group(['prefix' => 'auth'], function () use ($router) {
+            $router->post('/register', 'VacanciesController@getVacanciesList');
+            $router->post('/login', 'VacanciesController@getVacanciesList');
+            $router->post('/logout', 'VacanciesController@getVacanciesList');
+        });
 
-    $router->group(['prefix' => 'vacancy'], function () use ($router){
-        $router->get('/', 'VacanciesController@getVacanciesList');
-        $router->get('/{id}', 'VacanciesController@getVacancy');
-        $router->post('/', 'VacanciesController@createVacancy');
-        $router->put('/{id}', 'VacanciesController@updateVacancy');
-    });
-});
+        $router->group(['prefix' => 'vacancy'], function () use ($router) {
+                $router->get('/', 'VacanciesController@getVacanciesList');
+                $router->get('/{id}', 'VacanciesController@getVacancy');
+                $router->post('/', 'VacanciesController@createVacancy');
+                $router->put('/{id}', 'VacanciesController@updateVacancy');
+            }
+        );
+    }
+);
