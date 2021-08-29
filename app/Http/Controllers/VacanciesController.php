0f5324c707ccb5c951f7958fdb54d538955cfc3f
lmz2k <gabriel1997lima@gmail.com>
2021-08-29 11:28:49 -0300

    Creating controllers to get, create and update vacancy

--- app/Http/Controllers/VacanciesController.php
+++ app/Http/Controllers/VacanciesController.php
@@ -3,6 +3,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Services\Vacancy\VacancyServiceInterface;
@@ -11,45 +12,154 @@ class VacanciesController extends Controller
 {
     private VacancyServiceInterface $vacancyService;
 
-    public function __construct( VacancyServiceInterface $vacancyService)
+    public function __construct(VacancyServiceInterface $vacancyService)
     {
         $this->vacancyService = $vacancyService;
     }
 
     public function getVacanciesList(Request $request): JsonResponse
     {
-        $this->validate($request, [
-            'search' => 'string',
-            'hiring_mode' => 'in:PJ,CLT,BOTH',
-            'occupation' => 'in:BACK,FRONT,FULL',
-            'is_home_office' => 'bool',
-            'city_id' => 'integer',
-            'salary' => 'in:asc,desc',
-            'created_at' => 'in:asc,desc',
-            'page' => 'required|integer',
-            'per_page' => 'required|integer'
-        ]);
+        try {
+            $this->validate(
+                $request,
+                [
+                    'search' => 'string',
+                    'hiring_mode' => 'in:PJ,CLT,BOTH',
+                    'occupation' => 'in:BACK,FRONT,FULL',
+                    'is_home_office' => 'bool',
+                    'city_id' => 'integer',
+                    'salary' => 'in:asc,desc',
+                    'created_at' => 'in:asc,desc',
+                    'page' => 'required|integer',
+                    'per_page' => 'required|integer'
+                ]
+            );
 
-        $search = $request->input('search');
-        $hiringMode = $request->input('hiring_mode');
-        $occupation = $request->input('occupation');
-        $isHomeOffice = $request->input('is_home_office');
-        $cityId = $request->input('city_id');
+            $search = $request->input('search');
+            $hiringMode = $request->input('hiring_mode');
+            $occupation = $request->input('occupation');
+            $isHomeOffice = $request->input('is_home_office');
+            $cityId = $request->input('city_id');
+            $salary = $request->input('salary');
+            $createdAt = $request->input('created_at');
+            $page = $request->input('page');
+            $perPage = $request->input('per_page');
+
+            $result = $this->vacancyService->getVacanciesList(
+                $search,
+                $hiringMode,
+                $occupation,
+                $isHomeOffice,
+                $cityId,
+                $salary,
+                $createdAt,
+                $page,
+                $perPage
+            );
+
+            return response()->json($result, 200);
+        } catch (\Exception $e) {
+            return response()->json(
+                ['message' => 'Internal error'],
+                500
+            );
+        }
+    }
+
+    public function getVacancy(int $id, Request $request): JsonResponse
+    {
+        try {
+            $vacancyId = $id;
+
+            $result = $this->vacancyService->getVacancy($vacancyId);
+
+            return response()->json($result, 200);
+        } catch (\Exception $e) {
+            if ($e instanceof ModelNotFoundException) {
+                return response()->json(
+                    ['message' => 'Vacancy not found'],
+                    404
+                );
+            }
+            return response()->json(
+                ['message' => 'Internal error'],
+                500
+            );
+        }
+    }
+
+    public function createVacancy(Request $request): JsonResponse
+    {
+            $this->validate(
+                $request,
+                [
+                    'title' => 'string|required',
+                    'description' => 'string|required',
+                    'salary' => 'regex:/^\d*(\.\d{2})?$/|required',
+                    'is_home_office' => 'bool|required',
+                    'occupation' => 'in:BACK,FRONT,FULL|required',
+                    'city_id' => 'integer|required',
+                    'hiring_mode' => 'in:PJ,CLT,BOTH|required',
+                ]
+            );
+
+            $userId = 1;
+            $title = $request->input('title');
+            $description = $request->input('description');
+            $salary = $request->input('salary');
+            $isHomeOffice = $request->input('is_home_office');
+            $occupation = $request->input('occupation');
+            $cityId = $request->input('city_id');
+            $hiringMode = $request->input('hiring_mode');
+
+            $result = $this->vacancyService->createVacancy(
+                $userId,
+                $title,
+                $description,
+                $salary,
+                $isHomeOffice,
+                $occupation,
+                $cityId,
+                $hiringMode,
+            );
+
+            return response()->json($result, 200);
+    }
+
+    public function updateVacancy(Request $request): JsonResponse
+    {
+        $this->validate(
+            $request,
+            [
+                'id' => 'integer|required',
+                'title' => 'string',
+                'description' => 'string',
+                'salary' => 'regex:/^\d*(\.\d{2})?$/',
+                'is_home_office' => 'bool',
+                'occupation' => 'in:BACK,FRONT,FULL',
+                'city_id' => 'integer',
+                'hiring_mode' => 'in:PJ,CLT,BOTH',
+            ]
+        );
+
+        $id = $request->input('id');
+        $title = $request->input('title');
+        $description = $request->input('description');
         $salary = $request->input('salary');
-        $createdAt = $request->input('created_at');
-        $page = $request->input('page');
-        $perPage = $request->input('per_page');
+        $isHomeOffice = $request->input('is_home_office');
+        $occupation = $request->input('occupation');
+        $cityId = $request->input('city_id');
+        $hiringMode = $request->input('hiring_mode');
 
-        $result = $this->vacancyService->getVacanciesList(
-            $search,
-            $hiringMode,
-            $occupation,
-            $isHomeOffice,
-            $cityId,
+        $result = $this->vacancyService->updateVacancy(
+            $id,
+            $title,
+            $description,
             $salary,
-            $createdAt,
-            $page,
-            $perPage
+            $isHomeOffice,
+            $occupation,
+            $cityId,
+            $hiringMode,
         );
 
         return response()->json($result, 200);
