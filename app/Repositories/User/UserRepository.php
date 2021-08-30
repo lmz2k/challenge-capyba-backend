91ab90dcfc12d9dbcc3560dbd56fe4c6ee5d79ac
lmz2k <gabriel1997lima@gmail.com>
2021-08-30 00:06:01 -0300

    Adding method docs on UserRepository.php

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -9,12 +9,20 @@ use App\Models\User;
 class UserRepository implements UserRepositoryInterface
 {
 
+    /**
+     * @param $email
+     * @return mixed
+     */
     public function findUserByEmail($email)
     {
         return User::where('email', $email)
             ->firstOrFail();
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function findUserById($id)
     {
         return User::where('id', $id)
