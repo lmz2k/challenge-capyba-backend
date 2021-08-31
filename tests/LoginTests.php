801fb2d81491a4776f48f4809e0cea669055fa13
lmz2k <gabriel1997lima@gmail.com>
2021-08-30 23:42:04 -0300

    adding comments on login tests

--- tests/LoginTests.php
+++ tests/LoginTests.php
@@ -33,6 +33,7 @@ class LoginTests extends TestCase
 
     public function testLoginWithInvalidEmail()
     {
+        // hit api to login with email no registered on database
         $this->post($this->url . '/login', [
             'email' => 'teste@teste.com',
             'password' => $this->defaultPassword,
@@ -40,12 +41,14 @@ class LoginTests extends TestCase
 
         $data = json_decode($this->response->getContent(), true);
 
+        // confirm email doesnt exists
         $this->assertResponseStatus(404);
         $this->assertEquals('Email not registered on system', $data['message']);
     }
 
     public function testLoginWithoutEmailConfirmation()
     {
+        // hit api to login with email not confirmed yet
         $response = $this->post($this->url . '/login', [
             'email' => $this->defaultEmail,
             'password' => $this->defaultPassword,
@@ -53,14 +56,17 @@ class LoginTests extends TestCase
 
         $data = json_decode($this->response->getContent(), true);
 
+        // check if api returns a message saying to validate code received on email
         $this->assertResponseStatus(403);
         $this->assertEquals('Email not verified, new code has been sent to email', $data['message']);
     }
 
     public function testLoginWithWrongPassword()
     {
+        // set user verified attribute true, to pass throw code validation middleware
         $this->testDatabaseCreationHelper->updateUser($this->user->id, 'verified', 1);
 
+        // hit api trying to login with wrong password
         $this->post($this->url . '/login', [
             'email' => $this->defaultEmail,
             'password' => 'abcdef',
@@ -68,14 +74,18 @@ class LoginTests extends TestCase
 
         $data = json_decode($this->response->getContent(), true);
 
+        // check if api return message saying thar user sent a wrong password
         $this->assertResponseStatus(403);
         $this->assertEquals('Wrong password', $data['message']);
     }
 
     public function testLoginWithEmailAlreadyConfirmed()
     {
+        // set user verified attribute true, to pass throw code validation middleware
         $this->testDatabaseCreationHelper->updateUser($this->user->id, 'verified', 1);
 
+        // hit api trying to login with correct email and password
+        // check if api return status 200
         $this->post($this->url . '/login', [
             'email' => $this->defaultEmail,
             'password' => $this->defaultPassword,
