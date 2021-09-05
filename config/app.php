dbc192a1b1406043a7ec204612024e217a26658f
lmz2k <gabriel1997lima@gmail.com>
2021-09-05 18:22:16 -0300

    make it better

--- config/app.php
+++ config/app.php
@@ -3,5 +3,9 @@
 return [
     'aliases' => [
         'PDF' => Barryvdh\DomPDF\Facade::class,
-    ]
+    ],
+
+    'key' => env('APP_KEY', 'HqaujguiBrhEyAMyOBUz/PCGFTtFJYcXKW1CnFAUldo='),
+
+    'cipher' => 'AES-256-CBC',
 ];
